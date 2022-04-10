8aa9ffb4c717b413d1a36a59f0984eb26d6b2aea
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-04-10 13:11:13 +0300

    feat:display the quotes details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,12 @@
 export class Quote {
+    id: number;
+    quote: string;
+    like: number;
+    dislike: number;
+    constructor(id:number, quote:string, like: number, dislike: number){
+        this.id = id;
+        this.quote = quote;
+        this.like= like;
+        this.dislike = dislike;
+    }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  styleUrls: ['./quotes.component.css'],
 })
 export class QuotesComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, 'my first quote', 0, 0),
+    new Quote(2, 'my second quote', 0, 0),
+    new Quote(3, 'my third quote', 0, 0),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
