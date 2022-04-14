e8875d3a8f4cb9581ef7ffe7bd836032904faa55
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-04-14 08:15:16 +0300

    add the date count pipe

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -13,8 +13,15 @@ export class QuoteFormComponent implements OnInit {
   @Output() addQuote = new EventEmitter<Quote>();
 
   submitQuote() {
+    if (!this.newQuote){
+      alert("This is required")
+      
+    }else {
+
+    
     this.addQuote.emit(this.newQuote);
     this.newQuote = new Quote('', '', 0, 0, new Date());
+    }
   }
   constructor() {}
 
